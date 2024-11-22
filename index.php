bc29037524c743902fd3b527131ab8a913b41ca3
Alrizal2801 <alrizalaruftiansorullah@gmail.com>
2024-11-22 07:55:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$nama   =   "Al Rizal Arufti Ansorullah";
+$Kelas  =   "TI.23.CA1";
+$Nim    =   "312310682";
+$Prodi  =   "Teknik Informatika";
+
+?>
+
+<h1>
+    Saya Adalah
+    <?php echo $nama?>
+</h1>
+<p>
+    <strong>Kelas</strong>=<?php echo $Kelas?>
+</p>
+<p>
+    <strong>Nim</strong>=<?php echo $Nim?>
+</p>
+<p>
+    <strong>Prodi</strong>=<?php echo $Prodi?>
+</p>
